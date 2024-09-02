eaf1465ec47b095f495ee0815d4d818c623ba856
mrmalvicino <mrmalvicino@gmail.com>
2024-09-02 03:11:28 -0300

    Listar array de tipo Persona

--- src/ejercicio1/Archivo.java
+++ src/ejercicio1/Archivo.java
@@ -6,19 +6,22 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Archivo
 {
 	private String ruta;
+	private static int cantidadDeRegistros;
 
 	public Archivo()
 	{
-
+		cantidadDeRegistros = 0;
 	}
 
 	public Archivo(String ruta)
 	{
 		this.ruta = ruta;
+		cantidadDeRegistros = 0;
 	}
 
 	public boolean existe(String ruta)
@@ -146,5 +149,108 @@ public class Archivo
 	public void setRuta(String ruta)
 	{
 		this.ruta = ruta;
+	}
+
+	public int getCantidadDeRegistros()
+	{
+		setCantidadDeRegistros();
+		return cantidadDeRegistros;
+	}
+
+	private void setCantidadDeRegistros()
+	{
+		FileReader fr;
+		
+		try
+		{
+			fr = new FileReader(ruta);
+			BufferedReader bfr = new BufferedReader(fr);
+			String linea = "";
+			
+			while(linea != null)
+			{
+				cantidadDeRegistros++;
+				linea = bfr.readLine();
+			}
+			
+			bfr.close();
+			fr.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	// Lista todas las personas, sin filtrar repetidos ni invalidos
+	public ArrayList<Persona> listarGentes()
+	{
+		setCantidadDeRegistros();
+		ArrayList<Persona> gentes = new ArrayList<Persona>(cantidadDeRegistros);
+		
+		FileReader fr;
+		
+		try
+		{
+			fr = new FileReader(ruta);
+			BufferedReader bfr = new BufferedReader(fr);
+			String linea = "";
+			linea = bfr.readLine();
+			
+			while(linea != null)
+			{
+				String nombre = "";
+				String apellido = "";
+				String dinii = "";
+				
+				nombre = readCSVdata(0, linea);
+				apellido = readCSVdata(1, linea);
+				dinii = readCSVdata(2, linea);
+
+				Persona persona = new Persona(nombre, apellido, dinii);
+				gentes.add(persona);
+				
+				linea = bfr.readLine();
+			}
+			
+			bfr.close();
+			fr.close();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return gentes;
+	}
+	
+	private String readCSVdata(int column, String str)
+	{
+		ArrayList<String> arr = new ArrayList<String>(3);
+		
+		arr.add("");
+		arr.add("");
+		arr.add("");
+
+		int index = 0;
+		String aux = "";
+		
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str.charAt(i) != '-')
+			{				
+				aux += str.charAt(i);
+			}
+			else
+			{
+				arr.set(index, aux);
+				index ++;
+				aux="";
+			}
+		}
+		
+		arr.set(index, aux);
+		
+		return arr.get(column);
 	}
 }
--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,13 +1,14 @@
 package ejercicio1;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Principal
 {
 	public static void main(String[] args)
 	{	
 		Archivo archivo = new Archivo("Personas.txt");
-			
+
 		if(archivo.existe("Personas.txt"))
 		{
 			archivo.leerLineas();
@@ -18,8 +19,23 @@ public class Principal
 			System.out.println("Archivo creado");
 		}
 		
+		System.out.println("Cantidad de registros: " + archivo.getCantidadDeRegistros());
+
+		ArrayList<Persona> gentes = new ArrayList<Persona>(archivo.getCantidadDeRegistros());
+		gentes = archivo.listarGentes();
+		
+		System.out.println("Registro 8:");
+		System.out.println(gentes.get(7).toString()); // invalido
+		
+		System.out.println("Registro 2:");
+		System.out.println(gentes.get(1).toString()); // repedito
+		
+		System.out.println("Registro 4:");
+		System.out.println(gentes.get(3).toString()); // retepido
+
+		System.out.println("Carlitos:");
 		Persona carlitos = new Persona("Carlos", "Berlinguieri", "1234prueba");
-		System.out.println(carlitos.toString());
+		System.out.println(carlitos.toString() + "\n");
 		
 		try
 		{
